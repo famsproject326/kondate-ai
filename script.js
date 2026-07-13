27183ba8e277865d1e631b5265f14f9cf67d5f2a
famsproject326 <famsproject326@gmail.com>
2026-07-13 21:48:17 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ function createMenu() {
   const family = document.getElementById("family").value;
   const budget = document.getElementById("budget").value;
   const time = document.getElementById("time").value;
+  const child = document.getElementById("child").value;
+
+  let main = "";
+  let side = "";
+  let soup = "";
+
+
+  if (child === "あり") {
+
+    main = "🍖 甘辛チキン";
+    side = "🌽 コーン入りサラダ";
+    soup = "🍲 優しい味の味噌汁";
+
+  } else {
+
+    main = "🍖 鶏むね照り焼き";
+    side = "🥗 キャベツサラダ";
+    soup = "🍲 味噌汁";
+
+  }
+
 
   document.getElementById("menu").innerHTML =
 
@@ -11,23 +32,24 @@ function createMenu() {
 
     <h2>🍳 今日のおすすめ献立</h2>
 
-    <p>🍖 鶏むね照り焼き</p>
-    <p>🥗 キャベツサラダ</p>
-    <p>🍲 味噌汁</p>
+    <p>${main}</p>
+    <p>${side}</p>
+    <p>${soup}</p>
 
     <hr>
 
     <p>👨‍👩‍👧‍👦 家族人数：${family}人</p>
     <p>💰 予算：${budget}円以内</p>
     <p>⏰ 時間：${time}分以内</p>
+    <p>👧 子ども向け：${child}</p>
 
     <h3>🛒 買い物リスト</h3>
 
     <p>
     ☑ 鶏むね肉<br>
     ☑ キャベツ<br>
+    ☑ コーン<br>
     ☑ 玉ねぎ<br>
-    ☑ 味噌
     </p>
 
   </div>
@@ -42,6 +64,7 @@ function createWeek() {
   const family = document.getElementById("family").value;
   const budget = document.getElementById("budget").value;
   const time = document.getElementById("time").value;
+  const child = document.getElementById("child").value;
 
 
   document.getElementById("menu").innerHTML =
@@ -51,34 +74,20 @@ function createWeek() {
 
     <h2>📅 1週間献立</h2>
 
-    <p>月 🍖 鶏むね照り焼き</p>
-    <p>火 🍛 節約カレー</p>
-    <p>水 🐟 焼き魚定食</p>
+    <p>月 🍖 甘辛チキン</p>
+    <p>火 🍛 子どもカレー</p>
+    <p>水 🐟 魚の照り焼き</p>
     <p>木 🍳 オムライス</p>
     <p>金 🥘 豚丼</p>
     <p>土 🍝 パスタ</p>
     <p>日 🍲 鍋料理</p>
-
 
     <hr>
 
     <p>👨‍👩‍👧‍👦 家族人数：${family}人</p>
     <p>💰 予算：${budget}円以内</p>
     <p>⏰ 時間：${time}分以内</p>
-
-
-    <h3>🛒 まとめ買いリスト</h3>
-
-    <p>
-    ☑ 鶏むね肉<br>
-    ☑ 豚肉<br>
-    ☑ 卵<br>
-    ☑ キャベツ<br>
-    ☑ 玉ねぎ<br>
-    ☑ 米<br>
-    ☑ 調味料
-    </p>
-
+    <p>👧 子ども向け：${child}</p>
 
   </div>
   `;
