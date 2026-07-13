dec9b91805ed8ba738a3aa25dd1452ccc90c7512
famsproject326 <famsproject326@gmail.com>
2026-07-13 18:37:57 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function createMenu() {
 
   const food = document.querySelector("input").value;
-
   const family = document.getElementById("family").value;
-
   const budget = document.getElementById("budget").value;
-
   const time = document.getElementById("time").value;
 
   let main = "";
@@ -13,31 +10,44 @@ function createMenu() {
   let soup = "";
   let shopping = "";
 
+
   if (food.includes("鶏") || food.includes("肉")) {
 
     main = "🍖 鶏むね照り焼き";
     side = "🥗 キャベツサラダ";
     soup = "🍲 味噌汁";
+
     shopping = "☑ 鶏むね肉<br>☑ キャベツ<br>☑ 玉ねぎ<br>☑ 味噌";
 
-  } else if (food.includes("卵")) {
+  } 
+
+  else if (food.includes("卵")) {
 
     main = "🍚 親子丼";
     side = "🥗 ほうれん草のおひたし";
     soup = "🍲 豆腐味噌汁";
+
     shopping = "☑ 卵<br>☑ 玉ねぎ<br>☑ 豆腐";
 
-  } else if (food.includes("キャベツ")) {
+  }
 
-  main = "🥬 キャベツ炒め";
-  side = "🥗 キャベツサラダ";
-  soup = "🍲 味噌汁";
+  else if (food.includes("キャベツ")) {
 
-  shopping = "☑ キャベツ<br>☑ 豚肉<br>☑ 味噌";
+    main = "🥬 キャベツ炒め";
+    side = "🥗 キャベツサラダ";
+    soup = "🍲 味噌汁";
 
-}
+    shopping = "☑ キャベツ<br>☑ 豚肉<br>☑ 味噌";
 
-    
+  }
+
+  else {
+
+    main = "🍛 節約カレー";
+    side = "🥗 野菜サラダ";
+    soup = "🍎 フルーツ";
+
+    shopping = "☑ 肉類<br>☑ 野菜<br>☑ 調味料";
 
   }
 
@@ -56,9 +66,9 @@ function createMenu() {
     <hr>
 
     <p>👨‍👩‍👧‍👦 家族人数：${family}人</p>
-    <p>💰 目安：${budget}円以内</p>
-    <p>⏰ 調理時間：${time}分</p>
-    
+    <p>💰 予算：${budget}円以内</p>
+    <p>⏰ 調理時間：${time}分以内</p>
+
     <h3>🛒 買い物リスト</h3>
     <p>${shopping}</p>
 
